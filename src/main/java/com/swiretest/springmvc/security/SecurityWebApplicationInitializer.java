e58d74760b0eab08ca3bdd264ab9bc29f0aadfb2
guyuefeng <43138755@qq.com>
2017-10-01 21:49:48 +0800

    Spring Oauth2.0

---
+++ src/main/java/com/swiretest/springmvc/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.swiretest.springmvc.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
